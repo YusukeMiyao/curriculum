4f75c3de812cd9281774a4dace69364ad1f22f2f
YusukeMiyao <68069726+s12301800780@users.noreply.github.com>
2020-08-20 17:15:14 +0900

    Update Task.java

--- JavaStudy/2-3/src/study/Task.java
+++ JavaStudy/2-3/src/study/Task.java
@@ -13,8 +13,9 @@ public class Task extends Calculator {
 
 
 
-System.out.println ("plusメソッドの引数が一つの場合;"+plus(10));
-System.out.println("plusメソッドの引数が二つの場合;"+plus(10,20));
-System.out.println("plusメソッドの引数が三つの場合;"+plus(10,20,30));
+System.out.println ("plusメソッドの引数が一つの場合;"+super.plus(10));
+System.out.println("plusメソッドの引数が二つの場合;"+super.plus(10,20));
+System.out.println("plusメソッドの引数が三つの場合;"+super.plus(10,20,30));
+
     }
 }
