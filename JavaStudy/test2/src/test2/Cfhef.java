251ea1cc74b564cb2e525194eae88d4826517ace
YusukeMiyao <68069726+s12301800780@users.noreply.github.com>
2020-08-23 18:11:53 +0900

    Create Cfhef.java

---
+++ JavaStudy/test2/src/test2/Cfhef.java
@@ -0,0 +1,15 @@
+package test2;
+
+public class Cfhef {
+	public String specialized_field;
+
+	public Cfhef(String specialized_field_data) {
+		this.specialized_field=specialized_field_data;
+
+	}
+
+	public String grill(String ingredients) {
+		return ingredients+"グリル";
+	}
+
+}
