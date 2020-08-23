a34fbb718584913b91bbe8e827d305d9be8b6381
YusukeMiyao <68069726+s12301800780@users.noreply.github.com>
2020-08-23 18:12:18 +0900

    Create Check.java

---
+++ JavaStudy/Check2　/src/check/Check.java
@@ -0,0 +1,28 @@
+package check;
+
+public class Check {
+	private static String firstName="Yusuke";
+	private static String lastName="Miyao";
+
+	private static String printName (String firstName,String lastName) {
+return firstName+lastName;
+	}
+	public static void main(String[] args) {
+		System.out.println("printNameメソッド→"+printName(firstName,lastName));
+
+		Pet pt =new Pet(constants.Constants.CHECK_CLASS_JAVA,constants.Constants.CHECK_CLASS_HOGE);
+
+pt.introduce();
+
+RobotPet tp =new RobotPet("R2D2","ルーク");
+
+
+tp.introduce();
+
+
+
+	}
+
+    }
+
+
